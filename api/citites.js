31857e37ce7c96e8b3b4254fa88a7effa864e315
Maurice Drew <drewm1987@gmail.com>
2025-07-03 19:32:55 -0500

    added more code to my api for cities and also npm installed seed, cors, and schema to look at my table in psql

--- api/citites.js
+++ api/citites.js
@@ -10,3 +10,19 @@ router.route("/").get(async(req, res)=>{
     res.send(cities);
 });
 
+// GET cities/:ID
+router.route("/:id").get(async(req, res)=>{
+    const id = req.params.id;
+
+    if(isNaN(id) || id < 0){
+        return res.status(400).send(`Please use a valid number for the ID.`)
+    };
+
+    const city = await getCity(id);
+
+    if(!city){
+        return res.status(404).json(`City not found.`)
+    };
+
+    res.send(city);
+});
