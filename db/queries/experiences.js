e69773dce305e13334a9483a9b239cd71a3bde1d
Austina White <austinawhite@gmail.com>
2025-07-07 14:13:58 -0400

    routes for experiences

--- db/queries/experiences.js
+++ db/queries/experiences.js
@@ -11,3 +11,28 @@ export async function createExperiences({experience_name, experience_description
     return experiences[0];
     
 }
+
+// get all experiences 
+export async function getExperiences(){
+    const sql = `
+    SELECT * FROM experiences;`;
+    const {rows: experiences} = await db.query(sql)
+    return experiences;
+}
+
+
+// get all experiences for a city 
+export async function getExperiencesByCity(experience_city){
+    const sql = `
+    SELECT * FROM experiences WHERE experience_city = $1`;
+    const {rows: experiences} = await db.query(sql, [experience_city])
+    return experiences;
+}
+
+// get all experiences for a city, in a specific category 
+export async function getExperiencesByCityCat(experience_city, experience_category){
+    const sql = `
+    SELECT * FROM experiences WHERE experience_city = $1 AND experience_category = $2`;
+    const {rows: experiences} = await db.query(sql, [experience_city, experience_category])
+    return experiences;
+}
