08e5488c67653ee3f9f947164628cbaf008bd1cd
Austina White <austinawhite@gmail.com>
2025-07-10 18:14:03 -0400

    fixes for experience preview

--- api/experiences.js
+++ api/experiences.js
@@ -10,6 +10,61 @@ router.route("/").get(async(req,res)=>{
     res.send(experiences);
 });
 
+//get experience by an id 
+
+router.route("/details/:id").get(async(req, res)=>{
+
+    try {
+    const id = req.params.id;
+
+        console.log("Experience ID:", id);
+
+       if(id < 0){
+            return res.status(400).send(`Please use a valid city id.`);}
+    
+        const experiences = await getExperiencesById(parseInt(id));
+
+        console.log("Found experience:", experiences);
+    
+        if(!experiences){
+            return res.status(404).json(`Experience not found`)
+        };
+    
+        res.send(experiences);
+
+
+     } catch (error) {
+        console.error("Error in /experiences/:id route:", error);
+        res.status(500).json({ error });
+    }
+
+});
+
+// GET all experience for a specific city and category 
+
+router.route("/:city/:category").get(async(req, res)=>{
+    const experience_city = parseInt(req.params.city);
+    const experience_category = parseInt(req.params.category);
+
+    if(isNaN(experience_city) || experience_city < 0){
+        return res.status(400).send('Please use a valid city ID');
+    }
+
+    if(isNaN(experience_category) || experience_category < 1){
+        return res.status(400).send('Please use a valid category ID');
+    }
+
+    const experiences = await getExperiencesByCityCat(experience_city, experience_category);
+
+    if (!experiences || experiences.length===0){
+        return res.status(404).json(`There are no experiences in this category for ${experience_city}`)
+    }
+
+    res.send(experiences);
+});
+
+
+
 // GET all experience for a city 
 
 router.route("/:city").get(async(req, res)=>{
@@ -35,55 +90,3 @@ router.route("/:city").get(async(req, res)=>{
 });
 
 
-// GET all experience for a specific city and category 
-
-router.route("/:city/:category").get(async(req, res)=>{
-    const experience_city = parseInt(req.params.city);
-    const experience_category = parseInt(req.params.category);
-
-    if(isNaN(experience_city) || experience_city < 0){
-        return res.status(400).send('Please use a valid city ID');
-    }
-
-    if(isNaN(experience_category) || experience_category < 1){
-        return res.status(400).send('Please use a valid category ID');
-    }
-
-    const experiences = await getExperiencesByCityCat(experience_city, experience_category);
-
-    if (!experiences || experiences.length===0){
-        return res.status(404).json(`There are no experiences in this category for ${experience_city}`)
-    }
-
-    res.send(experiences);
-});
-
-//get experience by an id 
-
-router.route("/experiences/:id").get(async(req, res)=>{
-
-    try {
-    const id = req.params.id;
-
-        console.log("Experience ID:", id);
-
-       if(id < 0){
-            return res.status(400).send(`Please use a valid city id.`);}
-    
-        const experiences = await getExperiencesById(parseInt(id));
-
-        console.log("Found experience:", experiences);
-    
-        if(!experiences || experiences.length ===0){
-            return res.status(404).json(`Experience not found`)
-        };
-    
-        res.send(experiences);
-
-
-     } catch (error) {
-        console.error("Error in /experiences/:id route:", error);
-        res.status(500).json({ error });
-    }
-
-});
