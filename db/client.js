6d5a84a16711c7371b4faf2755c645e9942fb400
Austina White <austinawhite@gmail.com>
2025-07-17 19:16:56 -0400

    fix for client.js

--- db/client.js
+++ db/client.js
@@ -1,15 +1,6 @@
-import pg from "pg";
-const { Pool } = pg;
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Pool({
-  user: "postgres",
-  password: "abc123",
-  host: "localhost",
-  port: 5432,
-  database: "Capstone",
-  ssl: false,
-});
-
+import pg from "pg";
+const db = new pg.Client(process.env.DATABASE_URL);
 export default db;
